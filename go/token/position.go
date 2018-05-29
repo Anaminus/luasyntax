5e688d919de56f27acf301a243920884b74b2288
Anaminus <anaminus@protonmail.com>
2018-05-28 19:10:27 -0500

    Add documentation for token package.

--- go/token/position.go
+++ go/token/position.go
@@ -5,17 +5,27 @@ import (
 	"sync"
 )
 
+// A Position describes a position with a file, including the name of the
+// file, the line, and the column.
 type Position struct {
-	Filename string
-	Offset   int
-	Line     int
-	Column   int
+	Filename string // The name of the file, if specified.
+	Offset   int    // The offset within the file, starting at 0.
+	Line     int    // The line number, starting at 1.
+	Column   int    // The column number, starting at 1.
 }
 
+// IsValid returns whether the position is valid.
 func (pos *Position) IsValid() bool {
 	return pos.Line > 0
 }
 
+// String returns a formatted representation of the position, which may be one
+// of several forms:
+//
+// file:line:column    Filename with valid position.
+// line:column         No filename with valid position.
+// file                Filename with invalid position.
+// -                   No filename with invalid position.
 func (pos *Position) String() string {
 	s := pos.Filename
 	if pos.IsValid() {
@@ -27,12 +37,14 @@ func (pos *Position) String() string {
 	return s
 }
 
+// A File represents a Lua source file. Methods are safe to use concurrently.
 type File struct {
 	name  string
 	mutex sync.Mutex
 	lines []int
 }
 
+// NewFile creates a new file with the given name.
 func NewFile(filename string) *File {
 	return &File{
 		name:  filename,
@@ -40,10 +52,12 @@ func NewFile(filename string) *File {
 	}
 }
 
+// Name returns the name of the file.
 func (f *File) Name() string {
 	return f.name
 }
 
+// Returns the number of lines in the file.
 func (f *File) LineCount() (c int) {
 	f.mutex.Lock()
 	c = len(f.lines)
@@ -51,6 +65,7 @@ func (f *File) LineCount() (c int) {
 	return c
 }
 
+// AddLine specifies the location of the start of a line in the file.
 func (f *File) AddLine(offset int) {
 	f.mutex.Lock()
 	if i := len(f.lines); i == 0 || f.lines[i-1] < offset {
@@ -59,6 +74,7 @@ func (f *File) AddLine(offset int) {
 	f.mutex.Unlock()
 }
 
+// SetLinesForContent sets the line offset for a given file content.
 func (f *File) SetLinesForContent(content []byte) {
 	lines := []int{}
 	line := 0
@@ -77,6 +93,8 @@ func (f *File) SetLinesForContent(content []byte) {
 	f.mutex.Unlock()
 }
 
+// SearchInts performs a binary search for the nearest position of an int
+// within a slice of ints.
 func searchInts(a []int, x int) int {
 	// return sort.Search(len(a), func(i int) bool { return a[i] > x }) - 1
 	i, j := 0, len(a)
@@ -91,6 +109,7 @@ func searchInts(a []int, x int) int {
 	return i - 1
 }
 
+// Position returns the Position value for a given offset within the file.
 func (f *File) Position(offset int) (pos Position) {
 	pos.Offset = offset
 	pos.Filename = f.name
--- go/token/token.go
+++ go/token/token.go
@@ -1,10 +1,16 @@
+// The token package defines constants representing the lexical tokens of the
+// Lua programming language.
 package token
 
 import (
 	"strconv"
 )
 
+// Type indicates the type of a token.
 type Type int
+
+// Note: The list contains markers used only to indicate various classes of
+// tokens.
 
 const (
 	INVALID      Type = iota
@@ -89,63 +95,64 @@ const (
 )
 
 var tokens = [...]string{
-	INVALID:      "<invalid>",
-	EOF:          "<eof>",
-	SPACE:        "<space>",
-	COMMENT:      "<comment>",
-	LONGCOMMENT:  "<comment>",
-	NAME:         "<name>",
-	NUMBERFLOAT:  "<number>",
-	NUMBERHEX:    "<number>",
-	STRING:       "<string>",
-	LONGSTRING:   "<string>",
-	ADD:          "+",
-	SUB:          "-",
-	MUL:          "*",
-	DIV:          "/",
-	EXP:          "^",
-	MOD:          "%",
-	CONCAT:       "..",
-	LT:           "<",
-	LEQ:          "<=",
-	GT:           ">",
-	GEQ:          ">=",
-	EQ:           "==",
-	NEQ:          "~=",
-	SEMICOLON:    ";",
-	ASSIGN:       "=",
-	COMMA:        ",",
-	DOT:          ".",
-	COLON:        ":",
-	LBRACK:       "[",
-	RBRACK:       "]",
-	VARARG:       "...",
-	LPAREN:       "(",
-	RPAREN:       ")",
-	LBRACE:       "{",
-	RBRACE:       "}",
-	LENGTH:       "#",
-	DO:           "do",
-	END:          "end",
-	WHILE:        "while",
-	REPEAT:       "repeat",
-	UNTIL:        "until",
-	IF:           "if",
-	THEN:         "then",
-	ELSEIF:       "elseif",
-	ELSE:         "else",
-	FOR:          "for",
-	IN:           "in",
-	LOCAL:        "local",
-	FUNCTION:     "function",
-	RETURN:       "return",
-	BREAK:        "break",
-	NIL:          "nil",
-	FALSE:        "false",
-	TRUE:         "true",
-	AND:          "and",
-	OR:           "or",
-	NOT:          "not",
+	INVALID:     "<invalid>",
+	EOF:         "<eof>",
+	SPACE:       "<space>",
+	COMMENT:     "<comment>",
+	LONGCOMMENT: "<comment>",
+	NAME:        "<name>",
+	NUMBERFLOAT: "<number>",
+	NUMBERHEX:   "<number>",
+	STRING:      "<string>",
+	LONGSTRING:  "<string>",
+	ADD:         "+",
+	SUB:         "-",
+	MUL:         "*",
+	DIV:         "/",
+	EXP:         "^",
+	MOD:         "%",
+	CONCAT:      "..",
+	LT:          "<",
+	LEQ:         "<=",
+	GT:          ">",
+	GEQ:         ">=",
+	EQ:          "==",
+	NEQ:         "~=",
+	SEMICOLON:   ";",
+	ASSIGN:      "=",
+	COMMA:       ",",
+	DOT:         ".",
+	COLON:       ":",
+	LBRACK:      "[",
+	RBRACK:      "]",
+	VARARG:      "...",
+	LPAREN:      "(",
+	RPAREN:      ")",
+	LBRACE:      "{",
+	RBRACE:      "}",
+	LENGTH:      "#",
+	DO:          "do",
+	END:         "end",
+	WHILE:       "while",
+	REPEAT:      "repeat",
+	UNTIL:       "until",
+	IF:          "if",
+	THEN:        "then",
+	ELSEIF:      "elseif",
+	ELSE:        "else",
+	FOR:         "for",
+	IN:          "in",
+	LOCAL:       "local",
+	FUNCTION:    "function",
+	RETURN:      "return",
+	BREAK:       "break",
+	NIL:         "nil",
+	FALSE:       "false",
+	TRUE:        "true",
+	AND:         "and",
+	OR:          "or",
+	NOT:         "not",
+	// So that we don't get errors when iterating keywords.
 	unop_end:     "",
 	bool_start:   "",
 	bool_end:     "",
@@ -153,6 +160,7 @@ var tokens = [...]string{
 	binkey_end:   "",
 }
 
+// String returns a string representation of the token type, when possible.
 func (t Type) String() (s string) {
 	if 0 <= t && t < Type(len(tokens)) {
 		s = tokens[t]
@@ -163,46 +171,62 @@ func (t Type) String() (s string) {
 	return
 }
 
+// IsValid returns whether the type is valid.
 func (t Type) IsValid() bool {
 	return valid_start < t && t < valid_end
 }
 
+// IsPrefix returns whether the type is considered non-essential data. When
+// parsing, prefixes are held within a token structure as extra data that
+// precedes the main content.
+//
+// Spaces and comments are treated as prefixes.
 func (t Type) IsPrefix() bool {
 	return pre_start < t && t < pre_end
 }
 
+// IsComment returns whether the type indicates a comment.
 func (t Type) IsComment() bool {
 	return comm_start < t && t < comm_end
 }
 
+// IsNumber returns whether the type indicates a number value.
 func (t Type) IsNumber() bool {
 	return num_start < t && t < num_end
 }
 
+// IsString returns whether the type indicates a string value.
 func (t Type) IsString() bool {
 	return str_start < t && t < str_end
 }
 
+// IsOperator returns whether the type indicates an operator.
 func (t Type) IsOperator() bool {
 	return key_start < t && t < key_end
 }
 
+// IsUnary returns whether the type indicates a unary operator.
 func (t Type) IsUnary() bool {
 	return unop_start < t && t < unop_end
 }
 
+// IsKeyword returns whether the type indicates a keyword.
 func (t Type) IsKeyword() bool {
 	return key_start < t && t < key_end
 }
 
+// IsBool returns whether the type indicates a boolean value.
 func (t Type) IsBool() bool {
 	return bool_start < t && t < bool_end
 }
 
+// IsBinary returns whether the type indicates a binary operator.
 func (t Type) IsBinary() bool {
 	return (binop_start < t && t < binop_end) || (binkey_start < t && t < binkey_end)
 }
 
+// Precedence returns the priority of binary operators. The returned numbers
+// indicate the left and right priorities, respectively.
 func (t Type) Precedence() [2]int {
 	switch t {
 	case EXP:
@@ -223,6 +247,7 @@ func (t Type) Precedence() [2]int {
 	return [2]int{0, 0}
 }
 
+// UnaryPrecedence indicates the priority of unary operators.
 const UnaryPrecedence = 8
 
 var keywords map[string]Type
@@ -234,6 +259,7 @@ func init() {
 	}
 }
 
+// Lookup maps a name to its keyword, or NAME if it is not a keyword.
 func Lookup(name string) Type {
 	if t, ok := keywords[name]; ok {
 		return t
